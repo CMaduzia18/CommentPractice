252263512a0aac71d3e9df23f8ca8a942bea862b
student <windward_student@school.windwardschool.org>
2016-09-02 08:51:04 -0700

    Comment practice

---
+++ src/Pokeball.java
@@ -0,0 +1,30 @@
+
+public class Pokeball {
+	public static void main(String[] args){
+		System.out.println("  @@@@@@@");
+		System.out.println(" @@@@@@@@@@");
+		System.out.println("@@@@@@@@@@@@@");
+		System.out.println("|||||||||[\"\"\"\"]||||||||");
+		System.out.println("|||||||||[…….]|||||||||");
+		System.out.println("OOOOOOOOOOOOOOOOO");
+		System.out.println(" OOOOOOOOOOOOOOO");
+		System.out.println("   OOOOOOOOOOOOO");
+		System.out.println("     OOOOOOOOOO");
+	}//this pokeball graphic can be used in the opening sequence of my pokemon game,
+	//or any time the player need to catch a wild pokemon
+
+}
+/*If you want to
+ * leave a note for future programmers
+ * in with your code
+ * place two slashes, like this //
+ * in front of each line of your comment
+ * Another alternative is to
+ * enclose your comments with 
+ * /* but not \\*!
+ * 
+ * File name: Organization because that is the name of the class
+ * 
+ * 
+ */
+ 
